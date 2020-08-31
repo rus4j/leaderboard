59be17c94775a5e4d863d50ccb9b2d47bb66b043
rus4j <eto_ya08@mail.ru>
2020-08-31 23:22:59 +0300

    use separate select component

--- src/Settings/Settings.js
+++ src/Settings/Settings.js
@@ -1,6 +1,7 @@
 import React from "react";
 import DateSetting from "./DateSetting";
 import projects from "../sampleData/projects.json"; // /REST/API/1.0/PROJECTS?NAME&PERMISSION
+import Select from "../Select/Select";
 
 const styles = {
   margin: {
@@ -21,26 +22,13 @@ function Settings() {
         <input id="last_name" type="text" />
         <label htmlFor="last_name">Stash URL</label>
       </div>
-      <div className="input-field col s12">
-        <select>
-          <option defaultValue value="" disabled></option>
-          {projects.values.map((value, index) => {
-            return <option value={value.key}>{value.key}</option>;
-          })}
-        </select>
-        <label>Project</label>
-      </div>
+      <Select src={projects.values} label="Project" />
 
       {/* /REST/API/1.0/PROJECTS/{PROJECTKEY}/REPOS/{REPOSITORYSLUG}/BRANCHES?BASE&DETAILS&FILTERTEXT&ORDERBY&BOOSTMATCHES */}
-      <div className="input-field col s12">
-        <select>
-          <option defaultValue value="" disabled></option>
-          <option value="1">Option 1</option>
-          <option value="2">Option 2</option>
-          <option value="3">Option 3</option>
-        </select>
-        <label>Branch</label>
-      </div>
+      <Select
+        src={[{ key: "dev" }, { key: "master" }, { key: "release" }]}
+        label="Branch"
+      />
 
       <DateSetting></DateSetting>
 
