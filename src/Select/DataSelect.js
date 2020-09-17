68f1b0e8e5dca6737ff72d670a606017d307122f
Ruslan.Dibirov <Ruslan.Dibirov@nexign-systems.com>
2020-09-17 22:08:15 +0300

    fix styles for Select component

--- src/Select/DataSelect.js
+++ src/Select/DataSelect.js
@@ -1,16 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import { withStyles } from "@material-ui/core/styles";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
 import InputLabel from "@material-ui/core/InputLabel";
 
-function DataSelect({ src, label }) {
+const styles = (theme) => ({
+  input: {
+    color: theme.palette.common.white,
+    fontSize: 14,
+    "&:before": {
+      borderBottom: `1px solid ${"grey"} !important`,
+    },
+    marginBottom: 15,
+  },
+  labelText: {
+    color: "grey",
+    fontSize: 14,
+  },
+});
+
+function DataSelect(props) {
+  const { classes, src, label } = props;
   return (
     <>
-      <InputLabel id="select-label">{label}</InputLabel>
+      <InputLabel id="select-label" className={classes.labelText}>
+        {label}
+      </InputLabel>
       <Select
         labelId="select-label"
+        className={classes.input}
         // value={value}
         // onChange={handleChange}
       >
@@ -31,4 +51,4 @@ DataSelect.propTypes = {
   label: PropTypes.string.isRequired,
 };
 
-export default DataSelect;
+export default withStyles(styles)(DataSelect);
